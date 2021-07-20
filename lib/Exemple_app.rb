3e25a59dea7f624772269ceee39c506e58c78eaf
ecko9 <jean.ferstler@gmail.com>
2021-07-20 10:49:43 +0200

    Base files

---
+++ lib/Exemple_app.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'watir'
+require 'dotenv'
+require 'twitter'
+require 'rubocop'
+require 'launchy'
+
+
+####### FONCTION
+def fonction
+
+end
+
+
+####### PERFORM
+def perform
+
+end
+
+perform
